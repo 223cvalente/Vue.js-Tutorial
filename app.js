4b81aa0de27eda299e0f137a6665d7c62ea2e1da
chris <223cvalente@frhsd.com>
2023-03-17 09:13:00 -0400

    finished tutorial 2 and almost 3

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const app = Vue.createApp({
     //template: '<h2> I am the template </h2>'
     data(){
         return{
+          url:'https://www.thenetninja.co.uk',
             showbooks: true,
-            title: 'The Final Empire', 
-            author: 'Brandon Sanderson',
-            age: 45
+            books: [
+              {title: 'name of the wind' , author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+              {title: 'the way of the kings' , author: 'brandon sanderson',img: 'assets/2.jpg',isFav: false},
+              {title: 'the final empire' , author: 'brandon sanderson',img: 'assets/3.jpg',isFav: true},
+            ]
+            // title: 'The Final Empire', 
+            // author: 'Brandon Sanderson',
+            // age: 45,
+            // x:0,
+            // y:0
         }
     },
     methods:{
@@ -14,10 +22,20 @@ const app = Vue.createApp({
     //    //this.title = 'words of Randiance'
     //    this.title = title
     //   }   
-      toggleShowBooks(){
+      toggleShowBooks(e, data){
         this.showBooks= !this.showbooks
-      }
-      }
+      },
+    //   handleEvent(e, data){
+    //   console.log(e, e.type)
+    //   if(data){
+    //     console.log(data)
+    //   }
+    // },
+    //   handleMousemove(e){
+    //      this.x = e.offsetX
+    //      this.y = e.offsetY
+    //   }
+    }
 })
 
 app.mount('#app')
