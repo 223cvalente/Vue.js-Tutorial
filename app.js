37dc784dd3162291c48ce0c6aec77e99693de1d7
chris <223cvalente@frhsd.com>
2023-03-20 09:52:44 -0400

    finished #4

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = Vue.createApp({
           url:'https://www.thenetninja.co.uk',
             showbooks: true,
             books: [
-              {title: 'name of the wind' , author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
+              {title: 'name of the wind  ' , author: 'patrick rothfuss', img: 'assets/1.jpg', isFav: true},
               {title: 'the way of the kings' , author: 'brandon sanderson',img: 'assets/2.jpg',isFav: false},
               {title: 'the final empire' , author: 'brandon sanderson',img: 'assets/3.jpg',isFav: true},
             ]
@@ -39,3 +39,4 @@ const app = Vue.createApp({
 })
 
 app.mount('#app')
+
